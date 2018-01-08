bef0ad0e3dc36540392a29f611b83f961753fb20
xingliangbo <xingliangbo@u51.com>
2018-01-08 16:47:58 +0800

    init project

---
+++ src/main/java/com/liangbo/xing/xlbstudy/XlbstudyApplication.java
@@ -0,0 +1,12 @@
+package com.liangbo.xing.xlbstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlbstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XlbstudyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/liangbo/xing/xlbstudy/XlbstudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liangbo.xing.xlbstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XlbstudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
